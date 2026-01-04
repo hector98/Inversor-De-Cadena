e0eea98cb3a9d1e48ad028d543f69b010a93d23b
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2026-01-03 22:19:38 -0600

    modify lib/exeptions/invalid_string_error.rb

--- lib/exceptions/invalid_string_error.rb
+++ lib/exceptions/invalid_string_error.rb
@@ -2,5 +2,18 @@
 
 module Exceptions
   class InvalidStringError < StandardError
+    attr_reader :input, :error_code
+
+    def initialize(input, message = "Invalid String", error_code = :invalid_string)
+      @input = input
+      @error_code = error_code
+      super("#{message}: #{input}")
+    end
+  end
+
+  class NilStringError < InvalidStringError
+    def initialize(input)
+      super (input, "Not nil string", :nil_string)
+    end
   end
 end
